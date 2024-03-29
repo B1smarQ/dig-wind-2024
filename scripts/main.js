2960afd71af747be55dc5d196804f7efb527620d
B1smarQ <bgh050021@gmail.com>
2024-03-29 13:50:40 +0400

    дохера всего

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 function activate_menu(id){
     el = document.getElementById(id);
     el.style.visibility = "visible";
@@ -8,3 +9,19 @@ function deactivate_menu(id){
     el.style.visibility = "hidden";
     
 }
+let mybutton = document.getElementById("up_button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
