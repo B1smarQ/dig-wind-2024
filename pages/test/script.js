81802e14d55ce7b4aa2319b81ce73dac4c08f25e
B1smarQ <109068502+B1smarQ@users.noreply.github.com>
2024-04-26 09:29:14 +0400

    gallery fix 2

--- pages/test/script.js
+++ pages/test/script.js
@@ -1,6 +1,8 @@
 var track = document.getElementById("img-container");
+var link = document.getElementById("css-link");
 window.onmousedown = function (e) {
     track.dataset.mouseDown = e.clientX;
+    link === null || link === void 0 ? void 0 : link.setAttribute("href", "style.css");
 };
 window.onmousemove = function (e) {
     if ((track === null || track === void 0 ? void 0 : track.dataset.mouseDown) === '0')
@@ -17,10 +19,11 @@ window.onmousemove = function (e) {
         var image = _a[_i];
         image.animate({
             objectPosition: 100 + nextPercentage + "%" + " " + "50%"
-        }, { duration: 1200, fill: "forwards" });
+        }, { duration: 2000, fill: "forwards" });
     }
 };
 window.onmouseup = function (e) {
     track.dataset.mouseDown = '0';
     track.dataset.prevPercentage = track === null || track === void 0 ? void 0 : track.dataset.percentage;
+    link === null || link === void 0 ? void 0 : link.setAttribute("href", "style2.css");
 };
--- pages/test/script.ts
+++ pages/test/script.ts
@@ -1,6 +1,8 @@
 let track = document.getElementById("img-container");
+let link = document.getElementById("css-link");
 window.onmousedown = e =>{
     track.dataset.mouseDown = e.clientX;
+    link?.setAttribute("href","style.css");
 }
 window.onmousemove = e =>{
     if(track?.dataset.mouseDown === '0')return;
@@ -19,11 +21,12 @@ window.onmousemove = e =>{
     for(let image of track.getElementsByClassName("image")){
         image.animate({
             objectPosition : 100+nextPercentage + "%" + " " +"50%"
-        }, {duration:1200,fill: "forwards"});
+        }, {duration:2000,fill: "forwards"});
         
     }
 }
 window.onmouseup = e =>{
     track.dataset.mouseDown = '0';
     track.dataset.prevPercentage = track?.dataset.percentage;
+    link?.setAttribute("href","style2.css");
 }
