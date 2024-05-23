017028ef06dbe8a1c22466c090ca5a57627f242b
B1smarQ <bgh050021@gmail.com>
2024-05-23 18:31:24 +0400

    theme change

--- scripts/themeChange.js
+++ scripts/themeChange.js
@@ -1,20 +1,76 @@
-var currentTheme = sessionStorage.getItem('theme');
-currentTheme ? currentTheme : 'dark';
-function changeTheme() {
-    if (currentTheme == 'light') {
+let currentTheme = sessionStorage.getItem('theme');
+
+currentTheme?currentTheme:'dark';
+let swap_button = document.getElementById('theme_swap');
+let preloader = document.getElementById('preloader');
+const timer = ms => new Promise(res => setTimeout(res, ms))
+
+if(currentTheme == 'light'){
+    let cssLink = document.getElementById('style-link1');
+    let cssLink2 = document.getElementById('style-link2');
+    cssLink?.setAttribute('href','styleLight.css');
+    cssLink2?.setAttribute('href','styles/mainLight.css');
+}
+swap_button?.addEventListener('click', ()=>{changeTheme(['styleLight.css', 'styles/mainLight.css'], ['style.css', 'styles/main.css'])})
+function changeTheme(styleLight, styleDark){
+    console.log('swapping');
+    if(currentTheme == 'light'){
         currentTheme = 'dark';
     }
-    else {
+    else{
         currentTheme = 'light';
     }
-    sessionStorage.setItem('theme', currentTheme);
+    sessionStorage.setItem('theme',currentTheme);
+    swapStyle(styleLight, styleDark)
 }
-function swapStyle(styleLight, styleDark) {
-    var cssLink = document.getElementById('css-link');
-    if (currentTheme == 'light') {
-        cssLink === null || cssLink === void 0 ? void 0 : cssLink.setAttribute('href', styleLight);
+
+async function FadeIn(){
+    let opacity = 0;
+
+    preloader.style.display = 'block';
+    while(opacity <1){
+        
+        opacity +=0.005;
+        
+        preloader.style.opacity = String(opacity);
+        await timer(1);
     }
-    else {
-        cssLink === null || cssLink === void 0 ? void 0 : cssLink.setAttribute('href', styleDark);
+    
+}
+async function FadeOut(){
+    let opacity1 = 1;
+
+    while(opacity1 >0){
+       
+        opacity1 -=0.005;
+        
+            preloader.style.opacity = String(opacity1);
+
+            await timer(1);
+        
+        
+        
     }
+    preloader.style.display = 'none';
+   
+     
+    
+}
+function swapStyle(styleLight, styleDark ){
+    let cssLink = document.getElementById('style-link1');
+    let cssLink2 = document.getElementById('style-link2');
+    if(preloader.style.opacity <=0.05)
+    FadeIn();
+    setTimeout(()=>{
+        if(currentTheme == 'light'){
+            cssLink?.setAttribute('href',styleLight[0]);
+            cssLink2?.setAttribute('href',styleLight[1]);
+        }
+        else{
+            cssLink?.setAttribute('href',styleDark[0]);
+            cssLink2?.setAttribute('href',styleDark[1]);
+        }
+        FadeOut();
+    },1000)
+    
 }
--- scripts/themeChange.ts
+++ scripts/themeChange.ts
@@ -1,9 +1,11 @@
 let currentTheme = sessionStorage.getItem('theme');
 
 currentTheme?currentTheme:'dark';
-
-
-function changeTheme(){
+let swap_button = document.getElementById('theme_swap');
+let preloader = document.getElementById('preloader');
+swap_button?.addEventListener('click', ()=>{changeTheme(['styleLight.css', 'styles/mainLight.css'], ['style.css', 'styles/main.css'])})
+function changeTheme(styleLight:Array<string>, styleDark:Array<string>){
+    console.log('swapping');
     if(currentTheme == 'light'){
         currentTheme = 'dark';
     }
@@ -11,14 +13,51 @@ function changeTheme(){
         currentTheme = 'light';
     }
     sessionStorage.setItem('theme',currentTheme);
+    swapStyle(styleLight, styleDark)
 }
 
-function swapStyle(styleLight:string, styleDark:string ){
-    let cssLink = document.getElementById('css-link');
-    if(currentTheme == 'light'){
-        cssLink?.setAttribute('href',styleLight);
+function FadeIn(){
+    let opacity = 0;
+    console.log('fading in');
+    preloader.style.display = 'block';
+    if(opacity <1){
+        
+        opacity +=0.05;
+        setTimeout(() =>{FadeIn()},1)
+        
+    }
+    preloader?.style.opacity = String(opacity);
+}
+function FadeOut(){
+    let opacity = 1;
+    console.log('fading out');
+    if(opacity >0){
+       
+        opacity -=0.05;
+        setTimeout(()=>{FadeOut()},1)
+        
     }
     else{
-        cssLink?.setAttribute('href',styleDark);
+        preloader.style.display = 'none';
     }
+    preloader?.style.opacity = String(opacity);
+     
+    
+}
+function swapStyle(styleLight:Array<string>, styleDark:Array<string> ){
+    let cssLink = document.getElementById('style-link1');
+    let cssLink2 = document.getElementById('style-link2');
+    if(preloader.style.opacity <=0.05)
+    FadeIn();
+    
+    if(currentTheme == 'light'){
+        cssLink?.setAttribute('href',styleLight[0]);
+        cssLink2?.setAttribute('href',styleLight[1]);
+    }
+    else{
+        cssLink?.setAttribute('href',styleDark[0]);
+        cssLink2?.setAttribute('href',styleDark[1]);
+    }
+    if(preloader.style.opacity<=0.95)
+    FadeOut();
 }
